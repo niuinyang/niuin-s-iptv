d078e090d7162f2f0a330827d416639a85571db2
niuinyang <59207125+niuinyang@users.noreply.github.com>
2025-12-29 14:47:48 +0800

    Update A_generate_hash_workflows.py

--- scripts/A_generate_hash_workflows.py
+++ scripts/A_generate_hash_workflows.py
@@ -71,7 +71,7 @@ def generate_workflow(chunk_name, time_key, time_str, manual_count):
 
 on:
   workflow_run:
-    workflows: ["A生成并执行缓存workflow"]
+    workflows: ["A_生成并执行缓存workflow"]
     types:
       - completed
   workflow_dispatch:
