f7750c2173b4372af19b3bef29ddfb061b46d881
niuinyang <59207125+niuinyang@users.noreply.github.com>
2026-01-25 02:08:55 +0800

    Update 11_cvs_to_m3u.py

--- scripts/11_cvs_to_m3u.py
+++ scripts/11_cvs_to_m3u.py
@@ -119,9 +119,12 @@ def is_network_source(row, own_source_enabled):
 def can_output(row):
     s_reason = (row.get('静态筛除原因') or '').strip()
     l_reason = (row.get('轮播筛除原因') or '').strip()
+    audio = (row.get('音频') or '').strip()
     if s_reason not in ('未筛除', ''):
         return False
     if l_reason not in ('未筛除', ''):
+        return False
+    if audio not in ('有音频', ''):
         return False
     return True
 
